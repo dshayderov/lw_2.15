3b8820e52569080d90d1b1c5f48a4dce2f4cabc8
dshayderov <dshayderov@list.ru>
2022-11-23 00:48:53 +0300

    ind 2

--- Project/Индивидуальные задания/Индивидуальное задание 1/ind_1.py
+++ Project/Индивидуальные задания/Индивидуальное задание 1/ind_1.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+Написать программу, которая считывает текст из файла и определяет, сколько в нем слов,
+состоящих из не менее чем семи букв.
+"""
+
 
 if __name__ == "__main__":
     with open("ind_1.txt", "r", encoding="utf-8") as fileptr:
@@ -11,6 +16,5 @@ if __name__ == "__main__":
                 text = text.replace(i, "")
         words = text.split(" ")
 
-        print([word for word in words if len(word) > 6])
         s = len([word for word in words if len(word) > 6])
-        print(s)
+        print(f"Количество слов, состоящих из не менее 7 букв: {s}")
